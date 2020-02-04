fdbb151610710eb777a029a724cb8a0a51cc2c33
eoinm <eoinm@al.com.au>
2020-02-04 16:11:50 +1100

    get AL_USDUtils build working

---
+++ AL_USDUtils/package.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+name = 'AL_USDUtils'
+
+version = '0.2.0'
+
+private_build_requires = [
+    'AL_CMakeLib',
+    'cmake-2.8+',
+    'gcc-6.3.1',
+    'googletest-1.8',
+    'tbb-4.4+',
+]
+
+requires = [
+    'AL_boost-1.66',
+    'AL_boost_python-1.66',
+    'python-2.7',
+    'CentOS-7.4+<8',
+]
+
+variants = \
+    [['usdBase-0.19.7'],['usdBase-0.19.11']]
+
+def commands():
+    prependenv('LD_LIBRARY_PATH', '{root}/lib')
+    prependenv('CMAKE_MODULE_PATH', '{root}/cmake')
+    prependenv('PYTHONPATH', '{root}/lib/python')
